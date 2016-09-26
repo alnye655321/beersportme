688e2d698f300d179910980c22739a262cdbb3eb
Alex <alnye655321@gmail.com>
2016-09-26 16:47:39 -0600

    new build

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,15 @@ app.config(function($routeProvider) {
   $routeProvider.when('/swipe', {templateUrl:'swipe.html',  reloadOnSearch: false});
   $routeProvider.when('/forms', {templateUrl:'forms.html',  reloadOnSearch: false});
   $routeProvider.when('/profile', {templateUrl:'profile-form.html',  reloadOnSearch: false});
+  $routeProvider.when('/view_all_events', {templateUrl:'view_all_events.html',  reloadOnSearch: false});
+  $routeProvider.when('/view_all_sports', {templateUrl:'view_all_sports.html',  reloadOnSearch: false});
+  $routeProvider.when('/view_my_teams', {templateUrl:'view_my_teams.html',  reloadOnSearch: false});
+  $routeProvider.when('/view_all_teams', {templateUrl:'view_all_teams.html',  reloadOnSearch: false});
+  $routeProvider.when('/view_my_profile', {templateUrl:'view_my_profile.html',  reloadOnSearch: false});
+  $routeProvider.when('/view_my_calandar', {templateUrl:'view_my_calandar.html',  reloadOnSearch: false});
+  $routeProvider.when('/view_sports_map', {templateUrl:'view_sports_map.html',  reloadOnSearch: false});
+  $routeProvider.when('/add_event', {templateUrl:'add_event.html',  reloadOnSearch: false});
+  $routeProvider.when('/add_team', {templateUrl:'add_team.html',  reloadOnSearch: false});
 });
 
 
